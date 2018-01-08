5b37b144513dc5af2b5db69ca4692a29b4f2f12d
HoldYourWaffle <ravivanrooijen@live.nl>
2018-01-08 10:20:34 +0100

    Add default forge example mod

--- src/main/java/info/zthings/mcmods/examplemod/ExampleMod.java
+++ src/main/java/info/zthings/mcmods/examplemod/ExampleMod.java
@@ -1,6 +1,5 @@
 package info.zthings.mcmods.examplemod;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
